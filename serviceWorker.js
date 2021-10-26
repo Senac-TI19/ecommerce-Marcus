b051b1413a2ecfe114ed104a1b5fa41a1653497f
MarcusVinicius96 <marcus.araujo1996@hotmail.com>
2021-10-26 19:47:16 -0300

    colocado ./ nos ícones

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,11 +1,26 @@
 const cacheName = "cache-app-v1";
 
-const recursosCacheados = ["index.html",
-    "contato.html",
-    "pesquisar.html",
-    "style.css",
-    "favicon.ico",
-    "index.js"];
+const recursosCacheados = ["./",
+    "./index.html",
+    "./contato.html",
+    "./pesquisar.html",
+    "./style.css",
+    "./favicon.ico",
+    "./index.js",
+    "./icons/apple-icon-57x57.png",
+    "./icons/apple-icon-60x60.png",
+    "./icons/apple-icon-72x72.png",
+    "./icons/apple-icon-76x76.png",
+    "./icons/apple-icon-114x114.png",
+    "./icons/apple-icon-120x120.png",
+    "./icons/apple-icon-144x144.png",
+    "./icons/apple-icon-152x152.png",
+    "./icons/apple-icon-180x180.png",
+    "./icons/android-icon-192x192.png",
+    "./icons/favicon-32x32.png",
+    "./icons/favicon-96x96.png",
+    "./icons/favicon-16x16.png",
+    "./icons/icon-512x512.png"];
 
 self.addEventListener("install", function (event) {
     console.log("Service Worker instalado!");
