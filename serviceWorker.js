51d05f63181c60cb3c181b355cc091360330e298
MarcusVinicius96 <marcus.araujo1996@hotmail.com>
2021-10-27 21:18:48 -0300

    projeto ecommerce discos concluído

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,9 @@
 const cacheName = "cache-app-v1";
 
 const recursosCacheados = ["./",
+    "./compra.html",
+    "./minhaconta.html",
+    "./manifest.json",
     "./index.html",
     "./contato.html",
     "./pesquisar.html",
@@ -17,10 +20,16 @@ const recursosCacheados = ["./",
     "./icons/apple-icon-152x152.png",
     "./icons/apple-icon-180x180.png",
     "./icons/android-icon-192x192.png",
+    "./icons/ms-icon-144x144.png",
     "./icons/favicon-32x32.png",
     "./icons/favicon-96x96.png",
     "./icons/favicon-16x16.png",
-    "./icons/icon-512x512.png"];
+    "./icons/icon-512x512.png",
+    "./img/m_1025ca.jpg",
+    "./img/m_976ca.jpg",
+    "./img/Feliz-Da-Vida.jpg",
+    "./img/R-7338706-1464893930-8894.jpeg-300x300.jpg"
+    ];
 
 self.addEventListener("install", function (event) {
     console.log("Service Worker instalado!");
@@ -31,6 +40,20 @@ self.addEventListener("install", function (event) {
     );
 });
 
-self.addEventListener("activate", function () {
-    console.log("Service worker ativado!");
+self.addEventListener("fetch", function (event) {
+    console.log(`Request para o recurso ${event.request.url}`);
+
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            if (response){
+                console.log(`Recurso encontrado no cache: ${event.request.url}`)
+                return response;
+            } else {
+                console.log(`Recurso não encontrado no cache.
+                 Fazendo request para ${event.request.url}`);
+                 return fetch(event.request);
+            }
+        })
+    );
 });
+
